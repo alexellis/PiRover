02d0dc1ad71417af935174572f38487e8401125b
Alex Ellis <alexellis2@gmail.com>
2014-12-29 13:52:27 +0000

    keyboard

--- keyboard_drive.py
+++ keyboard_drive.py
@@ -5,19 +5,27 @@ rover = rover4wd()
 quit=False
 turnTime=0.8
 moveSpeed=1200
+moveTime=1
 while(quit==False):
 	sys.stdout.write( "Command: ")
 	line=raw_input()
 	if(line=='q'):
 		quit=True
 	elif(line=='w'):
-		rover.go('forwards', moveSpeed,1)
+		rover.go('forwards', moveSpeed,moveTime)
 	elif(line=='s'):
-		rover.go('reverse', moveSpeed, 1)
+		rover.go('reverse', moveSpeed, moveTime)
 	elif(line=='a'):
 		rover.go('left', 1000, turnTime)
 	elif(line=='d'):
 		rover.go('right', 1000, turnTime)
+	elif(line=="1"):
+		moveTime-=0.5
+		print "Move time: "+str(moveTime) +" seconds"
+	elif(line=="2"):
+		moveTime+=0.5
+		print "Move time: "+str(moveTime) +" seconds"
+
 	elif(line=="]"):
 		if(moveSpeed+100<2000):
 			moveSpeed+=100
